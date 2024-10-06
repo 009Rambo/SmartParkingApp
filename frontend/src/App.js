76dce014a3fa788f915ebd4b4852e260fbfb67c8
Srijala Poudel <srijana.poudel@tuni.fi>
2024-10-06 15:33:30 +0300

    Updated API key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,11 @@ import ParkingData from './ParkingData';
 import './App.css'; // If you created styles.css for styling
 
 function App(props) {
+    const style = {
+        width: '100%',
+        height: '50%'
+      };
+
     return (
         <div className="App">
             <h1>Smart Parking App</h1>
@@ -15,11 +20,12 @@ function App(props) {
                 google={props.google} // Access the google prop correctly
                 zoom={14}
                 initialCenter={{ lat: 61.4978, lng: 23.7610 }} // Initial center of the map (Tampere)
+                style={style}
             />
         </div>
     );
 }
 
 export default GoogleApiWrapper({
-    apiKey: "AIzaSyA2TefqyOcvJtx7AfLCf-jhvX9_WRpSFVs" // Replace with your actual API key
+    apiKey: process.env.REACT_APP_API_KEY // Replace with your actual API key
 })(App);
